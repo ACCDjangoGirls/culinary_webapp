709c98b8d2bd9ab8566764ef10318b90341766f3
Finnegan <chengdusean@gmail.com>
2025-04-03 12:25:48 -0400

    fixed errors with renaming

--- culinary/culinary/settings.py
+++ culinary/culinary/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = 'culinary.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = 'culinary.wsgi.application'
 
 
 # Database
