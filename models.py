0c41fc66fffc04d17fc1ff038836db2134a410b8
funneralplanner <plsdontatm3@gmail.com>
2025-04-07 11:53:39 -0400

    updated field types in models.py

--- models.py
+++ models.py
@@ -1,45 +1,37 @@
 from django.db import models
 
 class Reservation(models.Model):
-    partySize = models.CharField(max_length=3)
-    time = models.CharField(max_length=5)
+    partySize = models.PositiveSmallIntegerField()
+    dateTime = models.DateTimeField()
     
-    allergy = models.ForeignKey(Account, on_delete=models.CASCADE)
-    username = models.ForeignKey(Account, on_delete=models.CASCADE)
+    #allergy foreign key has been deleted. haven't made the Allergy model yet
     def __str__(self):
-        return self.partySize, self.allergy, self.time, self.username
-    
-
-class Account(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    allergy = models.CharField(max_length=250)
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username, self.password, self.email, self.allergy, self.firstName, self.lastName
-    
+        return (self.partySize, self.dateTime)
+       
 class Menu(models.Model):
     foodName = models.CharField(max_length=250)
     ingredients = models.ManyToManyField(Ingredient, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.foodName, self.ingredients
+        return (self.foodName, self.ingredients)
     
 class Ingredient(models.Model):
     ingredientName = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.ingredientName
+        return (self.ingredientName)
     
 class Order(models.Model):
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-    takeout = models.ForeignKey(Takeout, on_delete=models.CASCADE)
+    ORDER_TYPES = [
+        ('takeout','Takeout'),
+        ('dine-in','Dine_In'),
+        ('catering','Catering'),
+    ]
+    takeout = models.CharField(max_length=10, choices=ORDER_TYPES, default='dine-in')
 
     def __str__(self):
-        return self.reservation
+        return (self.reservation)
     
 class ItemsOrder(models.Model):
     foodName = models.ForeignKey(Menu, on_delete=models.CASCADE)
@@ -47,19 +39,19 @@ class ItemsOrder(models.Model):
     quantity = models.CharField(max_length=3)
 
     def __str__(self):
-        return self.foodName
+        return (self.foodName)
 
 class Takeout(models.Model):
     
     def __str__(self):
-        return self #ummmmmm help what goes in here/in this model??
+        return (self) #ummmmmm help what goes in here/in this model??
 
 class Event(models.Model):
     eventName = models.CharField(max_length=200)
-    startTime = models.CharField(max_length=5)
-    endTime = models.CharField(max_length=5)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
     location = models.CharField(max_length = 100)
-    eventDescription = models.CharField(max_length=2000)
+    eventDescription = models.TextField()
 
     def __str__(self):
-        return self.eventName, self.startTime, self.endTime, self.location, self.eventDescription
+        return (self.eventName)
