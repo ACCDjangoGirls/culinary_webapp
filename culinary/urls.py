186cca183094d1751caf3a2db69f515b07c6a545
Finnegan <chengdusean@gmail.com>
2025-04-09 12:30:35 -0400

    added urls.py

--- culinary/urls.py
+++ culinary/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="home.html")),
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
 ]
