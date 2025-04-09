336b016f7b1e76440250f6e8492556095f18aa0f
Lucy <itslucyday@gmail.com>
2025-04-09 18:38:25 -0400

    mixin view added

--- culinary/core/urls.py
+++ culinary/core/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-      path('', views.home, name='home'),  
+      path('', views.home, name='home'),
+      path('secret/', views.SecretIndexView.as_view(template_name="core/secret.html"), name ='secret'),
         ]
--- culinary/core/views.py
+++ culinary/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Ingredient
 # Create your views here.
 
 def home(request):
     return render(request, "home.html", {})
+
+class SecretIndexView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    template_name = 'secret.html'
 
