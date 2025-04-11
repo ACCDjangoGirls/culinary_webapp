be4746e79acc78dbd38b2dac4f59fc29ea2489e7
Chris Jones <christopher.jones@apsva.us>
2025-04-11 13:44:21 -0400

    ok, did a bunch of cleanup and bug fixes introduced by the merge. Ran the server and confirmed that it all works now!

--- culinary/urls.py
+++ culinary/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 from django.urls import path, include
 from core import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html")),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('core/', include('core.urls')),
+    path("", TemplateView.as_view(template_name="home.html")),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("core/", include("core.urls")),
 ]
