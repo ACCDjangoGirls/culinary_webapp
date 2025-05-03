5413227653b02e9346f0b41cd4f885712b023659
mulbah11k <mulbahtkolleh@gmail.com>
2025-05-03 10:41:53 +0000

    Register NewsImage as inline in NewsAdmin using TabularInline

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from .models import Menu, Ingredient, Order, ItemsOrder, Reservation, Event, News, NewsImage
 
-# class NewsImageInline(admin.TabularInline):
-#     model = NewsImage
-#     extra = 1
-
-# class NewsAdmin(admin.ModelAdmin):
-#     inlines = [NewsImageInline]
-
 admin.site.register(Reservation)
 admin.site.register(Ingredient)
 admin.site.register(Menu)
